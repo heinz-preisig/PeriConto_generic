79b4cdac0762cad5c820cd7bc981aeab849d7e20
Heinz A Preisig <heinz.preisig@chemeng.ntnu.no>
2025-02-28 12:08:47 +0100

    fix [brick & trees] * items -> camel case * primitive adding fixed

--- src/DataModel.py
+++ src/DataModel.py
@@ -243,10 +243,9 @@ class DataModel:
   def copyBrick(self, brickORtrees, oldName, newName):
     if brickORtrees == "bricks":
       what_graphs = self.BRICK_GRAPHS
-    elif brickORtrees == "trees":
+    else:# brickORtrees == "trees":
       what_graphs = self.TREE_GRAPHS
     self.newBrickOrTreeGraph(brickORtrees, newName)
-    # self.BRICK_GRAPHS[newName] = Graph()
     new_graph = what_graphs[newName]
     self.namespaces[newName] = Namespace(makeItemURI(newName, newName))  # Namespace(makeClassURI(newName))
     old_graph = what_graphs[oldName]
